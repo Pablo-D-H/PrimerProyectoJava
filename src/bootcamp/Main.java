ea52d406dcaab3e6cc4e1a8ae86652a8b196f956
Pablo-D-H <plopez@digitalhouse.com>
2022-01-19 17:14:27 -0300

    Primer proyecto en Java.

---
+++ src/bootcamp/Main.java
@@ -0,0 +1,25 @@
+package bootcamp;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) { //psvm
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingrese un nombre: ");
+        String nombre = teclado.nextLine();
+
+        System.out.print("Ingrese la fecha de hoy: ");
+        int numero = teclado.nextInt();
+
+        System.out.print("Es soleado por ahi?: ");
+        boolean comprobar = teclado.nextBoolean();
+
+        System.out.println("Hola " + nombre + ", como estas?"); //sout
+        System.out.println("La fecha de hoy es: " + numero);
+        System.out.println("Es soleado? "+ comprobar);
+
+
+    }
+}
